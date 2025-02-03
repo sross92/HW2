b35e828ff608785227d036fc044e47bbe23b295f
sross92 <sean@voyconstruction.com>
2025-02-02 21:03:35 -0600

    a

--- stem/hw2b.py
+++ stem/hw2b.py
@@ -17,7 +17,7 @@ def main():
     """
        fn1:  x-3cos(x)=0; with x0=1, x1= 2, maxiter = 5 and xtol = 1e-4
        fn2:  cos(2x)*x**3; with x0=1, x1= 2, maxiter = 15 and xtol = 1e-8
-       fn2:   with x0=1, x1= 2, maxiter = 3 and xtol = 1e-8
+       fn2:  cos(2x) * x^3; with x0=1, x1= 2, maxiter = 3 and xtol = 1e-8
 
        I observe that for functions 2 and 3, the answer should be pi/2 or about 1.57
     :return: nothing, just print results
